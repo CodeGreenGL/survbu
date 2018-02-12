824f8f1b9c6a234d7f64544dea7092d9b01caa88
Pavol Vasko <info@pavolvasko.co.uk>
2018-02-12 15:23:30 +0000

    Revert "Delete some files"
    This reverts commit 077cc1862982a6ea97618e16a6f10d6f853080a9.

---
+++ code/production/survbu_v1/survbu_v1/www/js/app_specific/sections/sections.service.js
@@ -0,0 +1,82 @@
+/*global angular */
+(function () {
+    'use strict';
+
+    angular
+        .module('surveyModule')
+        .factory('sectionsSrvc', sectionsSrvc);
+
+    sectionsSrvc.$inject = [
+        '$q', // promises service
+        '$http' // HTTP service
+    ];
+
+    function sectionsSrvc(
+        $q,
+        $http
+    ) {
+        //get all sections from codegreen restlet; returns deferred promise
+        var sectionsArray = [],
+            waitingState = true, // Set waitingstate to false so surveys load
+            service = {},
+            getAllSections = function (surveySections) {
+                var deferred = $q.defer();
+                if (surveySections.length > 0) {
+                    for (var i = 0, len = surveySections.length; i < len; i++) {
+                        $http({
+                            url: 'https://codegreen.restlet.net/v1/surveySections/' + surveySections[i],
+                            headers: {
+                                "authorization": "Basic OTQwZjRjNDctOWJjMS00N2E5LTgxZWQtMWNmMmViNDljOGRlOmIzYWU4MTZiLTk1ZTUtNGMyNy1iM2ZjLWRkY2ZmNjZhYjI2Nw==",
+                                "content-type": "application/json",
+                                "accept": "application/json"
+                            }
+                        }).then(function successCallback(response) {
+                            sectionsArray.splice(surveySections.indexOf(response.data['id']), 0, response.data);
+                            if (sectionsArray.length == surveySections.length) {
+                                deferred.resolve(sectionsArray);
+                            }
+                        }, function errorCallback(response) {
+                            console.error('Error while fetching sections');
+                            console.error(response);
+                        });
+                    }
+                } else {
+                    deferred.resolve(sectionsArray);
+                }
+                return deferred.promise;
+            };
+
+        var promiseToUpdateSections = function (surveySections) {
+            // returns a promise
+            return getAllSections(surveySections);
+        };
+
+        service.updateSections = function (surveySections) {
+            sectionsArray = [];
+            return promiseToUpdateSections(surveySections);
+        };
+
+        service.getSections = function () {
+            return angular.copy(sectionsArray);
+        };
+
+        service.getNumSections = function () {
+            return sectionsArray.length;
+        };
+
+        service.getSectionAt = function (index) {
+            return angular.copy(sectionsArray[index]);
+        };
+
+        service.isWaiting = function (iWait) {
+            waitingState = iWait;
+        };
+
+        service.isItWaiting = function () {
+            return waitingState;
+        };
+
+        return service;
+
+    }
+}());
